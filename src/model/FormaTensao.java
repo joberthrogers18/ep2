3db9cb881ce337333d6fdffc1228c3b754fb4968
Joberth Rogers <joberth.rogers18@gmail.com>
2017-11-12 18:06:38 -0200

    Forma Tensao

---
+++ src/model/FormaTensao.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FormaTensao extends Calculos {
+	private double voltagem;
+	private static float fre_angular = (float) (3.14*2*60);
+	private double ang_fase;
+
+	public FormaTensao() {
+		this.voltagem = 0;
+		this.ang_fase = -180;
+	}
+	
+	public  List<Double>  formaGrafico(double voltagem, double angulo){
+		List<Double> lista = new ArrayList<>();
+		
+		setAmplitude(voltagem);
+		setAngulo(angulo);
+		
+		for (float i = -50; i < 50; i = (float) (i + 0.4)) {
+           
+			lista.add(getAmplitude()*(Math.cos(fre_angular*i + getAngulo())));
+        }
+		return lista;
+		
+	}
+	
+	
+}
